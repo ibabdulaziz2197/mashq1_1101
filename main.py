b2195ccdb46d13385d0ccf4af8849f3526ad1aa0
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2026-01-12 16:06:43 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+# # 1
+# my_tuple = ()
+#
+#
+# coordinates = (10, 20)
+# days = ("dushanba", "seshanba", "chorshanba")
+#
+# print(coordinates)
+# print(days)
+# # 2
+# # Bo'sh set yaratish
+# empty_set = set()  # {} emas, chunki {} bo'sh dictionary yaratadi
+# print(empty_set)  # set()
+#
+# # Elementlar bilan set yaratish
+# fruits = {"olma", "banan", "uzum"}
+# numbers = {1, 2, 3, 3}  # Takrorlanadigan elementlar olib tashlanadi
+#
+# print(fruits)  # {'olma', 'banan', 'uzum'}
+# print(numbers)  # {1, 2, 3}  # 3 faqat bir marta saqlanadi
+
+# 3
+# set1 = {1, 2, 3}
+# set2 = {3, 4, 5}
+# 
+# # Birlashtirish (union)
+# print(set1 | set2)  # {1, 2, 3, 4, 5}
+# 
+# # Kesishish (intersection)
+# print(set1 & set2)  # {3}
+# 
+# # Ayirish (difference)
+# print(set1 - set2)  # {1, 2}
+
+
